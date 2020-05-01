433ca0a7c8e78022c74f1a8ca4253a577e8af3ff
Tex <atexier.at@gmail.com>
2020-05-01 21:25:12 +0200

    Big bang

---
+++ Assets/Scripts/BigBang.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigBang : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject     m_PlanetPrefab;
+
+    [SerializeField]
+    private int            m_NbPlanet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < m_NbPlanet; i++)
+            Instantiate(m_PlanetPrefab, transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("coucou");
+
+            BigBangPlanet[] bbps = GetComponentsInChildren<BigBangPlanet>();
+
+            foreach (BigBangPlanet bbp in bbps)
+                bbp.Go();
+        }
+    }
+}
---
+++ Assets/Scripts/BigBangPlanet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigBangPlanet : MonoBehaviour
+{
+    private Vector3     m_Dir;
+    private bool        m_go = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float size = Random.Range(0.5f, 2.5f);
+        transform.localScale = new Vector3(size, size, size);
+
+        m_Dir = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
+        m_Dir.Normalize();
+
+        m_Dir *= Random.Range(0.1f, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(m_go)
+        {
+            transform.Translate(m_Dir);
+        }
+    }
+
+    public void Go()
+    {
+        m_go = true;
+    }
+}
