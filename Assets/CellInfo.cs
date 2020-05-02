8a425b806a4c8dc0f6c1134ccd21f599155c8b9a
Pikevchu <gallien@et.esiea.fr>
2020-05-02 03:52:38 +0200

    wip

---
+++ Assets/CellInfo.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CellInfo : MonoBehaviour
+{
+    public enum Content
+    {
+        empty,
+        vegetal,
+        vegetarian,
+        carnivorus
+    }
+
+    public enum Status
+    {
+        alive,
+        dead
+    }
+
+    public int X = 0;
+
+    public int Y = 0;
+
+    public Status m_Status = Status.dead;
+
+    private Button m_Button = null;
+
+    private Image m_Image = null;
+
+    private void Awake()
+    {
+        m_Button = GetComponent<Button>();
+
+        m_Image = GetComponent<Image>();
+    }
+
+    public void InitCell(Action<CellInfo> OnClick)
+    {
+        m_Button.onClick.AddListener(() => OnClick(this));
+    }
+
+    public void UpdateStatus(Status status)
+    {
+        m_Status = status;
+        switch (status)
+        {
+            case Status.alive:
+                m_Image.color = Color.white;
+                break;
+
+            case Status.dead:
+                m_Image.color = Color.black;
+                break;
+
+            default:
+                break;
+        }
+    }
+}
---
+++ Assets/GameOfLife.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GameOfLife : MonoBehaviour
+{
+    private List<CellInfo> Cells = new List<CellInfo>();
+
+    private CellInfo[,] CellsArray;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Cells = GetComponentsInChildren<CellInfo>().ToList();
+
+        CellsArray = new CellInfo[10, 10];
+
+        foreach (CellInfo cell in Cells)
+        {
+            CellsArray[cell.X, cell.Y] = cell;
+            cell.InitCell(SelectCell);
+        }
+    }
+
+    public void SelectCell(CellInfo cell)
+    {
+        switch (cell.m_Status)
+        {
+            case CellInfo.Status.alive:
+                cell.UpdateStatus(CellInfo.Status.dead);
+                break;
+
+            case CellInfo.Status.dead:
+                cell.UpdateStatus(CellInfo.Status.alive);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    private IEnumerator LifeLogic()
+    {
+        while (Application.isPlaying)
+        {
+            yield return new WaitForSeconds(5f);
+        }
+    }
+
+    private int GetNeighboursAmount(CellInfo cell)
+    {
+        int neighbours = 0;
+
+        return neighbours;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
